e632444c357608b4f8af85a34018f163bf871c99
abembde <amolbembde@gmail.com>
2014-08-25 11:32:35 +0530

    remaining files

--- src/training/ideas/java/matrix/MatrixAdd.java
+++ src/training/ideas/java/matrix/MatrixAdd.java
@@ -4,11 +4,20 @@ package training.ideas.java.matrix;
  * Created by idnamb on 22-08-2014.
  */
 public class MatrixAdd {
-    public static int[][] add(int[][] matrixA, int[][] matrixB) {
 
+        public static Object[] add(int[][] MatrixA, int[][] MatrixB) {
 
-        return new int[0][];
+            int MatrixC[][] = new int[MatrixA.length][MatrixB[0].length];
+
+            for (int counter1 = 0; counter1 < MatrixA.length; counter1++) {
+
+                for (int counter2 = 0; counter2 < MatrixB.length; counter2++) {
+
+                    MatrixC[counter1][counter2] = MatrixA[counter1][counter2] + MatrixB[counter1][counter2];
+                }
+            }
+
+            return MatrixC;
+        }
     }
-}
-
 
--- testsrc/training/ideas/java/matrix/MatrixAddTest.java
+++ testsrc/training/ideas/java/matrix/MatrixAddTest.java
@@ -1,17 +1,25 @@
 package training.ideas.java.matrix;
 
-import junit.framework.Assert;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by idnamb on 22-08-2014.
  */
+
 public class MatrixAddTest {
+
+    @Test
+
+
     public void addMatrix () {
 
         int MatrixA[][] = {{1,4,6},{2,5,9},{3,6,9}};
         int MatrixB[][] = {{8,7,6},{5,4,3},{8,3,1}};
         int MatrixC[][] = {{9,11,12},{7,9,12},{11,9,10}};
-        Assert.assertEquals(MatrixC,MatrixAdd.add(MatrixA,MatrixB));
+        Assert.assertArrayEquals(MatrixC, MatrixAdd.add(MatrixA, MatrixB));
+
     }
 
 }
